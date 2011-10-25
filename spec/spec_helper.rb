ce8fe2baac006c532e1d3338a29fd245e5eb84c6
Adrian Rolufs <arolufs@gmail.com>
2011-10-25 07:00:55 -0700

    Finished Sign-in and Sign-out exercises

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,13 @@ Spork.prefork do
       def test_sign_in(user)
         controller.sign_in(user)
       end
+      
+      def integration_sign_in(user)
+        visit signin_path
+        fill_in :email,    :with => user.email
+        fill_in :password, :with => user.password
+        click_button
+      end
     end
 end
 
@@ -102,5 +109,12 @@ RSpec.configure do |config|
   
   def test_sign_in(user)
     controller.sign_in(user)
+  end
+  
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button
   end
 end
